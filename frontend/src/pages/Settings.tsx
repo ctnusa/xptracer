2f1fed29bf2d4e8ca27a77cddff8f5c09c0009c9
ctnusa <nguyenchitam2603@gmail.com>
2025-02-25 19:52:14 -0800

    add shadCN ui

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "../hooks/useTheme";
+import { Button } from "@/components/ui/button"
 
 const Settings = () => {
   const { isDarkMode, toggleTheme } = useTheme();
@@ -8,6 +9,8 @@ const Settings = () => {
       {isDarkMode && <div className="w-10 h-10 text-white">"DARK MODE"</div>}
       {!isDarkMode && <div className="w-10 h-10 text-black">"WHITE MODE"</div>}
       <button className="w-50 h-5 bg-white text-black" onClick={toggleTheme}>Change Theme</button>
+
+      <Button className="bg-blue-600 text-white">Click me</Button>
     </div>
   );
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import graphqlLoader from 'vite-plugin-graphql-loader';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import graphqlLoader from "vite-plugin-graphql-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(),
-    graphqlLoader()
-  ],
-})
+  plugins: [react(), tailwindcss(), graphqlLoader()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
